8f0822ac1cbcf45121f45d6a1fdbaa98292a1980
Mostafa Faryabi <faryabi.mostafa@gmail.com>
2024-06-01 11:39:40 +0330

    query client + useQuery + queryClientProvider

--- frontRecactQuery/src/App.tsx
+++ frontRecactQuery/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+
+  const result = useQuery({queryKey : ['posts'] , queryFn : async function () {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    return data.json()
+  }})
+
+  console.log('reeeeeeeeeeeees here ' , result);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {result.isPending ? (<div>LOADIIIIIIIIIIIING</div>) : (
+    <ul>
+      {result.data.map((lis : any)=> <li key={lis.id}>{lis.title}</li>)}
+    </ul>  
+    )}
+    
     </>
   )
 }
--- frontRecactQuery/src/main.tsx
+++ frontRecactQuery/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries : {
+      refetchInterval : 5000,
+      staleTime:4000
+    }
+  }
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+      <ReactQueryDevtools/>
+    </React.StrictMode>
+  </QueryClientProvider>
+);
